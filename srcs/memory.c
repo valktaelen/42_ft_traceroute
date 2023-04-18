ab023de180adedd27546f508f934496ae3151499
valktaelen <alix.artiges@hotmail.fr>
2023-04-19 11:04:29 +1200

    norme

--- srcs/memory.c
+++ srcs/memory.c
@@ -73,7 +73,7 @@ int	ft_strcmp(const char *str1, const char *str2)
 	size_t	i;
 
 	if (!str1 || !str2)
-		return 1;
+		return (1);
 	i = 0;
 	while (str1[i] && str2[i] && str1[i] == str2[i])
 		++i;
